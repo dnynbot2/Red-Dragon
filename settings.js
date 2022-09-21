4db2460233930868cea16f97ac6d8d4a643344b4
dnynbot2 <114065789+dnynbot2@users.noreply.github.com>
2022-09-21 15:05:56 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -30,7 +30,7 @@ global.doc6 = 'application/vnd.android.package-archive'
 
 //owner v card
 global.vcardowner = ['+94741619883'] // owner number
-global.ownername = "😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯" // owner name
+global.ownername = "ᴍʀ.ᴅᴜʟᴀ" // owner name
 global.ytname = "" // yt chanel name
 global.socialm = "GitHub: prabhasha2006" // github or insta name
 global.location = "India, Mizoram, Aizawl" // location
